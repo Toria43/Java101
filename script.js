7b2e32b5fe679759742eba0e176f51c489e0a262
Victory <ahereosamudiamen@gmail.com>
2024-03-29 21:41:03 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,12 @@
+let js = 'amazing';
+console.log (40 + 8 + 23 - 10);
+
+let firstName = 'Victory';
+
+console.log(firstName);
+
+let victory_agholor = "VA";
+let $function = 40;
+
+let person = "victory"; 
+let PI = 3.1415;
