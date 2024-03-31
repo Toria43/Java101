eaa2be0c168615a27288dcf0dd6b261e960174f0
Victory <ahereosamudiamen@gmail.com>
2024-03-31 19:56:31 +0100

    completed values and variables

--- script.js
+++ script.js
@@ -5,8 +5,25 @@ let firstName = 'Victory';
 
 console.log(firstName);
 
-let victory_agholor = "VA";
+let victory_agholor = 'VA';
 let $function = 40;
 
-let person = "victory"; 
+let person = 'victory'; 
 let PI = 3.1415;
+
+let myFirstJob = 'Teacher';
+let myCurrentJob = 'Programmer';
+
+let country = 'Nigeria';
+let continent = 'Africa';
+let population = 100;
+
+console.log(victory_agholor);
+console.log($function);
+console.log(person);
+console.log(PI);
+console.log(myFirstJob);
+console.log(myCurrentJob);
+console.log(country);
+console.log(continent);
+console.log(population);
