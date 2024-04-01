90146a9b6d3eea52d893eef10c568a358dc42558
Victory <ahereosamudiamen@gmail.com>
2024-04-01 10:04:50 +0100

    first challenge done!

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ let firstName = 'Victory';
 
 console.log(firstName);
 
+// Variable name conventions
 let victory_agholor = 'VA';
 let $function = 40;
 
@@ -27,3 +28,48 @@ console.log(myCurrentJob);
 console.log(country);
 console.log(continent);
 console.log(population);
+
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+console.log(typeof true);
+
+let isIsland = false;
+let language;
+
+console.log(typeof isIsland);
+console.log(typeof population);
+console.log(typeof country);
+console.log(typeof language);
+
+const now = 2024;
+const ageDaniel = now - 2000;
+const ageChole = now - 2003;
+console.log(ageDaniel, ageChole);
+console.log(ageDaniel * 2, ageChole / 7, 4 ** 3);
+// 4 ** 4 means 4 raised to the power of 3 i.e. 4 * 4 * 4 
+
+const middleName = 'Ahere';
+const lastName = 'Agholor';
+console.log(middleName + ' ' + lastName);
+
+console.log(ageDaniel > ageChole);
+console.log(ageChole <= 10);
+
+console.log(now - 2000 > now - 2003);
+
+const averageAge = (ageDaniel + ageChole) / 2
+console.log(ageDaniel, ageChole, averageAge)
+
+/*the first challenge 😃*/
+let massMark = "95";
+let heightMark = "1.88";
+let massJohn = "85";
+let heightJohn = "1.76";
+
+let BMIMark = massMark / (heightMark * heightMark);
+let BMIJohn = massJohn / (heightJohn * heightJohn);
+
+console.log(BMIMark, BMIJohn);
+
+let markHigherBMI = BMIMark > BMIJohn;
+console.log(markHigherBMI);
